9e4dbcf8e839f9db838227dee229ab44a7ce67f4
Groga <groga65@gmail.com>
2018-10-18 00:00:12 +0300

    Первое задание с калькулятором, + - * / степень

--- Calculate.java
+++ Calculate.java
@@ -5,6 +5,7 @@ public class Calculate{
 	public static void main(String[] args){
 
 		System.out.println("Консольный калькулятор ....");
+		//Записываем числа в массив args под индексами 0 и 1. 			//т.к. массия является строчным то преобразуем его в 			//int (Integer.valueOf)
 		int frist = Integer.valueOf(args[0]);
 		int second = Integer.valueOf(args[1]);
 		int summ = frist + second;
@@ -15,6 +16,7 @@ public class Calculate{
 		System.out.println("Умножение = " + multiplication);
 		int division = frist / second;
 		System.out.println("Деление = " + division);
+		// Пришлось подсмотреть у интернета как возвести в 			// степень ))))
 		double power = Math.pow(frist, second);
 		System.out.println("Возведение в степень = " + power);
 	}
